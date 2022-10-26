7c0492aa16d2f5f83de6b1d9420fce952923204a
UraniumCheese <othiago_10_ago@hotmail.com>
2022-10-25 22:54:14 -0300

    Add large cactus
    Changes:
    
    - Made a list with the small cactuses and large cactuses lists
    - Used the random module to choose one of the cactus lists, and use that value for the cactus instead of SMALL_CACTUS

--- dino_runner/components/obstacles/obstacle_manager.py
+++ dino_runner/components/obstacles/obstacle_manager.py
@@ -1,14 +1,18 @@
 import pygame
+import random
 from dino_runner.components.obstacles.cactus import Cactus
-from dino_runner.utils.constants import SMALL_CACTUS
+from dino_runner.utils.constants import SMALL_CACTUS, LARGE_CACTUS
 
 class ObstacleManager:
     def __init__(self):
         self.obstacles = []
 
     def update(self, game):
+        available_cactuses = [SMALL_CACTUS, LARGE_CACTUS]
+        chosen_cactus = random.choice(available_cactuses)
+
         if len(self.obstacles) == 0:
-            self.obstacles.append(Cactus(SMALL_CACTUS))
+            self.obstacles.append(Cactus(chosen_cactus))
 
         for obstacle in self.obstacles:
             obstacle.update(game.game_speed, self.obstacles)
